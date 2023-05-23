0a87d82f4634a9ab7b0f1110dc79e4c292aabe01
Vinicius <viniciusjose9@outlook.com>
2023-05-23 18:07:18 -0300

    tela de message alterada

--- src/pages/message.jsx
+++ src/pages/message.jsx
@@ -1,7 +1,14 @@
 export default function Message() {
     return (
-        <div>
-            
-        </div>
-            ); 
+        <>
+            <div>
+                <h1>Mensagens Recebidas: </h1>
+                
+
+            </div>
+
+        </>
+
+    );
+
 }
